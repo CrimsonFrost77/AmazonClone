18979150333d31936462222d4555fde85f916e1f
CrimsonFrost77 <grayishbluegamer@gmail.com>
2024-11-15 08:59:40 +0600

    used product array from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,36 +1,5 @@
 const productsGrid = document.querySelector(".js-products-grid");
 
-// Array of products
-const products = [
-  {
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    price: 1090,
-    rating: {
-      value: 4.5,
-      count: 87,
-    },
-  },
-  {
-    name: "Intermediate Size Basketball",
-    image: "images/products/intermediate-composite-basketball.jpg",
-    price: 2095,
-    rating: {
-      value: 4.0,
-      count: 127,
-    },
-  },
-  {
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    price: 799,
-    rating: {
-      value: 4.5,
-      count: 56,
-    },
-  },
-];
-
 //generate products HTML from the products array
 let productsHTML = "";
 
